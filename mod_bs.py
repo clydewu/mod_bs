36bddfd392583e1595f649c3b97ecb95692eeca8
Clyde Wu <claudwu@gmail.com>
2020-05-26 21:24:57 +0800

    Lint

--- mod_bs.py
+++ mod_bs.py
@@ -13,16 +13,16 @@ def find_oldest_line(arr):
     while idx_s <= idx_e:
         idx_m = (idx_s + idx_e) // 2
 
-        #-- Check whether idx_m is the turning point
-        #-- Only check its prev/next number if this number at the end/start of the array
+        # -- Check whether idx_m is the turning point
+        # -- Only check its prev/next number if this number at the end/start of the array
         # print(f'{idx_s} -- {idx_m} -- {idx_e}')
         # print(f'{arr[idx_s]} -- {arr[idx_m]} -- {arr[idx_e]}')
         if (idx_m == 0 or arr[idx_m] < arr[idx_m - 1]) \
-                and (idx_m == len(arr) -1 or arr[idx_m] < arr[idx_m + 1]):
+                and (idx_m == len(arr) - 1 or arr[idx_m] < arr[idx_m + 1]):
             return idx_m
 
         if arr[idx_s] > arr[idx_m]:
-            idx_e = idx_m 
+            idx_e = idx_m
         elif arr[idx_m] > arr[idx_e]:
             idx_s = idx_m
         else:
@@ -65,10 +65,8 @@ def modified_binary_search(arr, val):
         elif arr[mid + 1] <= arr[end] and (arr[mid + 1] <= val and val <= arr[end]):
             begin = mid + 1
         elif arr[begin] > arr[mid - 1] and not (val > arr[mid - 1] and val < arr[begin]):
-            #if val > arr[mid - 1] and val < arr[begin]:
-            #    return None
             end = mid - 1
-        elif arr[mid + 1] > arr[end] and not ( val > arr[end] and val < arr[mid + 1]):
+        elif arr[mid + 1] > arr[end] and not (val > arr[end] and val < arr[mid + 1]):
             begin = mid + 1
         else:
             return None
@@ -86,4 +84,3 @@ def find_val(arr, val):
     elif arr[oldest_line] <= val and arr[-1] >= val:
         ret = binary_search(arr[oldest_line:], val)
         return ret + oldest_line if ret is not None else None
-
